2c51e2ba5d66a63d5b47837a6dd991546a649a09
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-19 17:56:42 +0530

    flutter

---
+++ lib/first_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Firstscreen extends StatelessWidget {
+  const Firstscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+class Firstscreen1 extends StatefulWidget {
+  const Firstscreen1({super.key});
+
+  @override
+  State<Firstscreen1> createState() => _Firstscreen1State();
+}
+
+class _Firstscreen1State extends State<Firstscreen1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
