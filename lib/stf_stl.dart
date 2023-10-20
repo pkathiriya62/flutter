e606046fe2104a7e376f29dadfae8110094a0b4e
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-20 17:49:51 +0530

    initstate_dispose

--- lib/stf_stl.dart
+++ lib/stf_stl.dart
@@ -17,6 +17,16 @@ class Firstscreen1 extends StatefulWidget {
 
 class _Firstscreen1State extends State<Firstscreen1> {
   @override
+  void initState() {
+    print("initstate");
+    super.initState();
+  }
+  @override
+  void dispose() {
+    print("dispose");
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
