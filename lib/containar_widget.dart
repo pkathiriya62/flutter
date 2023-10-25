7d50809263cbc47da43d7286ed837834b4bb45da
Prince kathiriya <princekathiriya62@gmail.com>
2023-10-25 18:51:03 +0530

    container

---
+++ lib/containar_widget.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:flutter/material.dart';
+
+class ContainerWidget extends StatefulWidget {
+  const ContainerWidget({super.key});
+
+  @override
+  State<ContainerWidget> createState() => _ContainerWidgetState();
+}
+
+class _ContainerWidgetState extends State<ContainerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.blue,
+        title: Center(child: Text('Hello')),
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
+          height: 200,
+          width:200,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+              ),
+            ],
+            //shape: BoxShape.circle,
+            border: Border.all(),
+            color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.circular(10),
+            ),
+          child: Center(child: Text('HELLO')),
+          ),
+      ),
+
+      );
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_project/containar_widget.dart';
 import 'package:first_project/stf_stl.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Firstscreen(),
+      home: const ContainerWidget (),
     );
   }
 }
