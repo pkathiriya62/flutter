cb7f5465770b667b72618705793a91ce49312001
Prince kathiriya <princekathiriya62@gmail.com>
2023-12-20 18:29:14 +0530

    box

---
+++ lib/gridviewbuild.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GridviewBuilder extends StatelessWidget {
+  const GridviewBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            mainAxisExtent: 100,
+          ),
+          itemCount: 21,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: const Center(
+                child: Text('data'),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
